945757b5ebf723130386aa6b52d6c3d38ea192ba
陈碎碎 <taixuling@gmail.com>
2023-09-28 16:11:20 +0800

    del: delete vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  build: {
-    transpile: ["vuetify"],
-  },
   modules: ["@nuxtjs/tailwindcss"],
   devtools: { enabled: true },
-  plugins: ["@/plugins/vuetify"],
-  css: [
-    "vuetify/lib/styles/main.sass",
-    "@mdi/font/css/materialdesignicons.min.css",
-  ],
 });
