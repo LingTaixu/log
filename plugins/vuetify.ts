0d91209434f2b0f40714524bef2c1171ce86c82d
陈碎碎 <taixuling@gmail.com>
2023-09-28 15:53:12 +0800

    CL: test

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -3,13 +3,12 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "vuetify/styles";
-import "vuetify/lib/components/VCode/VCode.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     directives,
-    ssr: true,
+    // ssr: true,
   });
 
   nuxtApp.vueApp.use(vuetify);
